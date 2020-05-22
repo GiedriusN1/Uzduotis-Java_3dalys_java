5a99040e3cbcdcbde28b6acdcadc45cb5bd98304
GiedriusNekrasas <giedrius.nekrasas@gmail.com>
2020-05-22 18:59:44 +0300

    Pradzia

--- src/main/java/lt/bit/java2/uzduotisjava_3dalys/entities/Product.java
+++ src/main/java/lt/bit/java2/uzduotisjava_3dalys/entities/Product.java
@@ -1,4 +1,21 @@
 package lt.bit.java2.uzduotisjava_3dalys.entities;
 
+
+import org.hibernate.annotations.Table;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Entity
+//@Table(name = "product")
+//@NamedEntityGraph(name = "products.stores", attributeNodes = @NamedAttributeNode("stores"))
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private String address;
+
 }
